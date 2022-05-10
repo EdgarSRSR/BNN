d65c246b74885ab56caec883c0562e80ca285974
edgarsr <edgarsolisromeu@gmail.com>
2022-05-10 19:16:31 +0700

    read and display dtata from MNIST

--- bnn_lenet.m
+++ bnn_lenet.m
@@ -10,38 +10,81 @@
 %
 %===============================================================================
 
-clear; clc; close all:
+%clear; clc; close all:
+clear;
 
 %========================== Constants Definition ===============================
 
-epochs = 400; %Number of epochs the network will be strained. For good results it must be 1000,  but we will start lower
-alpha = 0.2; %Learning rate for the training process
+%epochs = 400; %Number of epochs the network will be strained. For good results it must be 1000,  but we will start lower
+%alpha = 0.2; %Learning rate for the training process
 
-width=28; %Width of the picture
-height=28; %height of the picture
+%width=28; %Width of the picture
+%height=28; %height of the picture
 
 %======================= Constants Definition ==================================
 
 fprintf('Reading and Preparing Training Data  \n')
 
+data = load('../mnist/mnist_test.csv');
+size(data) 
+
+data(1,1)
+
+% get the labels
+labels = data(:,1);
+
+%labels'   %display all labels
+
+% get images without labels
+images = data(:,2:785);
+
+size(images)
+
+% get the values of the first image as a row.
+% images(1,:)
+
+% create a 28x28 matrix from the data of the first image.
+
+imgTest = reshape(images(1,:),28,28);
+% show the image for the first row
+colormap gray
+imagesc(reshape(images(1,:),28,28)')
+labels(1)
+
+% show image for the 100th row
+imagesc(reshape(images(100,:),28,28)')
+labels(100)
+
 %Picture that will be ued for training
-inputPicture = imread('');
+%inputPicture = imread('');
 %Picture with labels that corresponds to input
-labelPicture = imread('');
+%labelPicture = imread('');
 
 %Looking at the loaded pictures before training
-imshow(inputPicture);
-figure();
-imshow(labelPicture);
+%imshow(inputPicture);
+%figure();
+%imshow(labelPicture);
 
 %Prepare the data for the training
-inputPicture = cast(inputPicture, 'double'); %casting from uint into double
+%inputPicture = cast(inputPicture, 'double'); %casting from uint into double
 
 %Create a matrix with the dimensions of the picture for the later labelPicture
-labels = zeros(height,width);
+%labels = zeros(height,width);
 
 %==================================Generate Network=============================
-fprintf('Generate Network \n')
+%fprintf('Generate Network \n')
+
+% Kernel for convolution
+kernel = [0, 1; 0, 1]
+
+% work on binarizing the image
+binaryImage = []
+for i = imgTest
+  binaryImage(end + 1,:) =deterministic_binarization(i);
+ endfor
+ 
+ binaryImage 
+reshape(binaryImage(),28,28);
 
 % Define the network structure as a vector
 % [784 128 10]
@@ -49,46 +92,46 @@ fprintf('Generate Network \n')
 %  - 128 hidden layers
 %  - 10 output layers, because there are 10 classes where we can classify an image
 
-networkStructure = [784 128 10];
+%networkStructure = [784 128 10];
 
 %Create network
-network = generateNetwork(networkStructure);
+%network = generateNetwork(networkStructure);
 
 %=================================Training======================================
-fprintf('Start Training\n');
+%fprintf('Start Training\n');
 
 %Train the network.
-[trainedNetwork,costLog,accuracyLog]= trainedNetwork(inputPicture,labels,network,'epochs',epochs,'alpha',alpha);
+%[trainedNetwork,costLog,accuracyLog]= trainedNetwork(inputPicture,labels,network,'epochs',epochs,'alpha',alpha);
 
 %Plot the cost log from training
-figCostLog=figure();
-plot(costLog);
-ylabel('Loss');
-xlabel('epochs');
+%figCostLog=figure();
+%plot(costLog);
+%ylabel('Loss');
+%xlabel('epochs');
 
-fprintf('Training Done\n')
+%fprintf('Training Done\n')
 
 %============================Prediction=========================================
-fprintf('Using Trained Network for Test Prediciton\n')
+%fprintf('Using Trained Network for Test Prediciton\n')
 
 %Use the trained network on the inputPicture to see results
-predOutput = networkPrediciton(inputPicture, trainedNetwork);
+%predOutput = networkPrediciton(inputPicture, trainedNetwork);
 
 %============================Generate Results===================================
 
-fprintf('Results \n')
+%fprintf('Results \n')
 
 %Remove the last line from the first matrix, because those are wiegths for
 % connections that go to the bias in the hidden layer, which are not needed for VHDL IM=Mplementation
-nnParams = trainedNetwork;
-nnParams{1} = nnParams{1}(1:end-1,:); %ignore last column
+%nnParams = trainedNetwork;
+%nnParams{1} = nnParams{1}(1:end-1,:); %ignore last column
 
-frpintf('\nWeight Matrix from the Input to the HiddenLayer\n')
-disp(nnParams{1});
-fprintf('Weight Matrix from the Hidden to the Output Layer\n')
-disp(nnParams{2});
+%frpintf('\nWeight Matrix from the Input to the HiddenLayer\n')
+%disp(nnParams{1});
+%fprintf('Weight Matrix from the Hidden to the Output Layer\n')
+%disp(nnParams{2});
 
-save('bb_lenet.mat','trainedNetwork','networkStructure','nnParams');
-fprintf('\nFinished Script\n')
+%save('bb_lenet.mat','trainedNetwork','networkStructure','nnParams');
+%fprintf('\nFinished Script\n')
 
 
--- deterministic_binarization.m
+++ deterministic_binarization.m
@@ -7,10 +7,10 @@
 % DETERMINISTIC_BINARIZATION Biniarizes the value by turning it into a +1 value if it is equal or above 0
 % or to -1 if it below 0
 
-function v = sigmoid(x)
-  if (x >= 0)
-    v = 1
-  else
+function v = deterministic_binarization(x)
+  if (x == 0)
     v = -1
+  else
+    v = 1
 end
 
